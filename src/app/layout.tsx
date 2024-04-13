e39f0bb906e8a5fc82a2edf143b388eb8b78a783
Gyandeep Aryan <gyandeeparyan2@gmail.com>
2024-04-13 15:07:13 +0530

    navbar completed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+   
+    <html lang="en" className="dark">
+     <body>
+  <Navbar/>
+       {children}
+     </body>
+   </html>
+   
+   
   );
 }
